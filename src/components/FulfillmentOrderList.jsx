0cbafc93cd123f59a544c39ee94dc36d1a8be806
Megan Majewski <megan.majewski@shopify.com>
2022-03-14 13:50:37 -0400

    Connect fulfillmentCreatev2 to button for each fulfillment order

--- src/components/FulfillmentOrderList.jsx
+++ src/components/FulfillmentOrderList.jsx
@@ -1,9 +1,49 @@
+import { Button } from '@shopify/polaris';
+import { gql, useMutation } from '@apollo/client';
+
 export function FulfillmentOrderList({ fulfillmentOrders }) {
+  const FULFILLMENT_CREATE_MUTATION = gql`
+    mutation fulfillmentCreateV2($fulfillment: FulfillmentV2Input!) {
+      fulfillmentCreateV2(fulfillment: $fulfillment) {
+        fulfillment {
+          id
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  `;
+
+  const [createFulfillmentMutation] = useMutation(FULFILLMENT_CREATE_MUTATION);
   return (
     <>
-      {fulfillmentOrders.map(({ id }) => (
-        <p>{id}</p>
-      ))}
+      {fulfillmentOrders.map(({ id }) => {
+        return (
+          <>
+            <p>{id}</p>
+            <Button
+              onClick={async () => {
+                const result = await createFulfillmentMutation({
+                  variables: {
+                    fulfillment: {
+                      lineItemsByFulfillmentOrder: [
+                        {
+                          fulfillmentOrderId: id,
+                        },
+                      ],
+                    },
+                  },
+                });
+                console.log('result', result);
+              }}
+            >
+              Fulfill
+            </Button>
+          </>
+        );
+      })}
     </>
   );
 }
