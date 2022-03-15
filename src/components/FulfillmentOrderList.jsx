45e4c0a3c0f68a6f55e0e0ff8e859808876c76b0
Megan Majewski <megan.majewski@shopify.com>
2022-03-15 14:09:55 -0400

    fix set errror flag if mutation fails

--- src/components/FulfillmentOrderList.jsx
+++ src/components/FulfillmentOrderList.jsx
@@ -1,4 +1,10 @@
-import { Card, Button, IndexTable, Toast } from '@shopify/polaris';
+import {
+  Card,
+  Button,
+  IndexTable,
+  Toast,
+  useIndexResourceState,
+} from '@shopify/polaris';
 import { useFulfillmentCreateV2 } from '../hooks/useFulfillmentCreateV2';
 import { useState } from 'react';
 
@@ -7,61 +13,88 @@ export function FulfillmentOrderList({ fulfillmentOrders }) {
   const [fulfillmentOrdersState, setFulfillmentOrdersState] =
     useState(fulfillmentOrders);
   const [showError, setShowError] = useState(false);
+  const { selectedResources, handleSelectionChange } = useIndexResourceState(
+    fulfillmentOrdersState
+  );
 
-  const removeFulfillmentItemFromList = (id) => {
+  const removeFulfillmentItemFromList = (idsToRemove) => {
     setFulfillmentOrdersState((prev) => {
-      const itemToRemove = prev.findIndex((order) => order.id === id);
-      const newItems = [...prev];
-      newItems.splice(itemToRemove, 1);
-      return newItems;
+      const itemsNotFulfilled = prev.filter(
+        (order) => !idsToRemove.includes(order.id)
+      );
+      return itemsNotFulfilled;
     });
   };
+
+  const fulfillItems = async (ids) => {
+    const result = await createFulfillment(ids);
+    if (result.data.fulfillmentCreateV2?.userErrors.length) {
+      setShowError(true);
+      return;
+    }
+    removeFulfillmentItemFromList(ids);
+  };
+
   const fulfillmentOrderListItems = fulfillmentOrdersState.map(
     ({ id, assignedLocation, order }, index) => (
-      <IndexTable.Row id={id} key={id} position={index}>
+      <IndexTable.Row
+        id={id}
+        key={id}
+        position={index}
+        selected={selectedResources.includes(id)}
+      >
         <IndexTable.Cell>{id}</IndexTable.Cell>
         <IndexTable.Cell>{order.id}</IndexTable.Cell>
         <IndexTable.Cell>{assignedLocation.name}</IndexTable.Cell>
         <IndexTable.Cell>
-          <Button
-            onClick={async () => {
-              const result = await createFulfillment(id);
-              if (result.data?.useFulfillmentCreateV2?.userErrors.length) {
-                showError(true);
-              }
-              console.log('result', result);
-              removeFulfillmentItemFromList(id);
-            }}
-          >
-            Fulfill
-          </Button>
+          <Button onClick={() => fulfillItems([id])}>Fulfill</Button>
         </IndexTable.Cell>
       </IndexTable.Row>
     )
   );
 
   return (
-    <Card>
-      <Toast
-        content="Error - not able to perform mutation"
-        error
-        onDismiss={() => setShowError(false)}
-      />
-      <IndexTable
-        resourceName={{
-          singular: 'Fulfillment Order',
-          plural: 'Fulfillment Orders',
-        }}
-        itemCount={Object.keys(fulfillmentOrdersState).length}
-        headings={[
-          { title: 'Fulfillment Order ID' },
-          { title: 'Order ID' },
-          { title: 'Location' },
-          { title: 'Fulfill Now' },
-        ]}
-      >
-        {fulfillmentOrderListItems}
-      </IndexTable>
-    </Card>
+    <>
+      {showError && (
+        <>
+          test
+          <Toast
+            content="Error - not able to perform mutation"
+            error
+            onDismiss={() => setShowError(false)}
+          />
+        </>
+      )}
+      <Card>
+        <IndexTable
+          resourceName={{
+            singular: 'Fulfillment Order',
+            plural: 'Fulfillment Orders',
+          }}
+          itemCount={Object.keys(fulfillmentOrdersState).length}
+          onSelectionChange={handleSelectionChange}
+          headings={[
+            { title: 'Fulfillment Order ID' },
+            { title: 'Order ID' },
+            { title: 'Location' },
+            { title: 'Fulfill Now' },
+          ]}
+        >
+          {fulfillmentOrderListItems}
+        </IndexTable>
+        {/*
+         We can decide to keep this or not - you cant fulfill across multiple orders so need to update hook
+        {selectedResources.length > 0 && (
+          <Button
+            primary
+            onClick={() => {
+              fulfillItems(selectedResources);
+            }}
+          >
+            Fulfill {selectedResources.length} items
+          </Button>
+        )} */}
+      </Card>
+    </>
   );
 }
